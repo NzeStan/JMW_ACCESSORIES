a44105cec312c0c47cf8ff857cc838817dbfb878
NzeStan <nnamaniifeanyi10@gmail.com>
2026-01-02 09:59:54 +0100

    still on it

--- order/admin.py
+++ order/admin.py
@@ -1,66 +1,86 @@
-# admin.py
 from django.contrib import admin
-from .models import BaseOrder, NyscKitOrder, NyscTourOrder, ChurchOrder, OrderItem
+from .models import Order, OrderItem
 
 
 class OrderItemInline(admin.TabularInline):
+    """Inline display of order items within an order"""
     model = OrderItem
-    raw_id_fields = ["content_type"]  # Remove object_id since it's a UUID
     extra = 0
-    readonly_fields = ["object_id"]  # Make object_id readonly instead
+    readonly_fields = ["content_type", "object_id", "price", "quantity"]
+    fields = ["content_type", "object_id", "price", "quantity", "extra_fields"]
+    can_delete = False
 
 
-@admin.register(BaseOrder)
-class BaseOrderAdmin(admin.ModelAdmin):
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    """Admin interface for unified Order model"""
     list_display = [
-        "serial_number",
+        "id",
+        "user",
         "first_name",
         "last_name",
         "email",
         "phone_number",
+        "status",
         "paid",
+        "total_cost",
         "created",
     ]
-    list_filter = ["paid", "created"]
-    search_fields = ["first_name", "last_name", "email", "serial_number"]
-    inlines = [OrderItemInline]
-
-
-@admin.register(NyscKitOrder)
-class NyscKitOrderAdmin(admin.ModelAdmin):
-    list_display = [
-        "serial_number",
+    list_filter = ["status", "paid", "created"]
+    search_fields = [
+        "id",
         "first_name",
         "last_name",
-        "state_code",
-        "state",
-        "local_government",
-        "paid",
+        "email",
+        "phone_number",
+        "user__email",
     ]
-    list_filter = ["paid", "state"]
-    search_fields = ["first_name", "last_name", "state_code", "serial_number"]
+    readonly_fields = ["id", "created", "updated", "total_cost"]
     inlines = [OrderItemInline]
+    
+    fieldsets = (
+        ("Order Information", {
+            "fields": ("id", "user", "status", "paid", "total_cost")
+        }),
+        ("Customer Details", {
+            "fields": ("first_name", "last_name", "email", "phone_number")
+        }),
+        ("Delivery Details", {
+            "fields": ("delivery_details",),
+            "description": "JSON field containing delivery-specific information"
+        }),
+        ("Timestamps", {
+            "fields": ("created", "updated"),
+            "classes": ("collapse",)
+        }),
+    )
+    
+    def get_queryset(self, request):
+        """Optimize queryset with related objects"""
+        return super().get_queryset(request).select_related("user").prefetch_related("items")
 
 
-@admin.register(NyscTourOrder)
-class NyscTourOrderAdmin(admin.ModelAdmin):
-    list_display = ["serial_number", "first_name", "last_name", "paid"]
-    list_filter = ["paid"]
-    search_fields = ["first_name", "last_name", "serial_number"]
-    inlines = [OrderItemInline]
-
-
-@admin.register(ChurchOrder)
-class ChurchOrderAdmin(admin.ModelAdmin):
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    """Admin interface for order items"""
     list_display = [
-        "serial_number",
-        "first_name",
-        "last_name",
-        "pickup_on_camp",
-        "delivery_state",
-        "delivery_lga",
-        "paid",
+        "id",
+        "order",
+        "content_type",
+        "object_id",
+        "price",
+        "quantity",
+        "get_total",
     ]
-    list_filter = ["paid", "pickup_on_camp", "delivery_state"]
-    search_fields = ["first_name", "last_name", "serial_number"]
-    inlines = [OrderItemInline]
+    list_filter = ["content_type"]
+    search_fields = ["order__id", "order__email"]
+    readonly_fields = ["get_total"]
+    
+    def get_total(self, obj):
+        """Calculate total cost for this item"""
+        return obj.get_cost()
+    get_total.short_description = "Total Cost"
+    
+    def get_queryset(self, request):
+        """Optimize queryset"""
+        return super().get_queryset(request).select_related("order", "content_type")
